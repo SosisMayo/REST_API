a3b946e21d6f8d0b44a8fcb62c2cdf9444f2f8db
SosisMayo <ramadani.dwicahya@gmail.com>
2021-04-17 21:37:18 +0700

    Commit Pertama Rest Server

---
+++ Rest-Server/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+const daftarMahasiswa = JSON.parse(fs.readFileSync('./data.json'));
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{ 
+    res.send("HALAMAN DEPAN");
+});
+
+//READ ALL
+app.get('/mahasiswa',(req,res)=>{
+    res.json(daftarMahasiswa);
+});
+
+//CREATE
+app.post('/mahasiswa',(req,res)=>{
+    const mahasiswa={
+        id:daftarMahasiswa.length+1,
+        nama:req.body.nama,
+        jurusan:req.body.jurusan,
+    }
+    daftarMahasiswa.push(mahasiswa);
+    res.send(mahasiswa);
+    fs.writeFileSync('./data.json',JSON.stringify(daftarMahasiswa));
+})
+
+//READ BY ID
+app.get('/mahasiswa/:id',(req,res)=>{
+    const mahasiswa = daftarMahasiswa.find(data=>data.id === parseInt(req.params.id));
+    if(!mahasiswa) res.status(404).send('Data mahasiswa dengan ID tersebut tidak ditemukan !');
+    res.json(mahasiswa);
+})
+
+//UPDATE
+app.put('/mahasiswa/:id',(req,res)=>{
+    const mahasiswa = daftarMahasiswa.find(data=>data.id === parseInt(req.params.id));
+
+    //jika data mhs yang akan diupdate tidak ada
+    if(!mahasiswa) res.status(404).send('Data mahasiswa dengan ID tersebut tidak ditemukan !');
+
+    //jika ditemukan
+    daftarMahasiswa[parseInt(req.params.id)-1].nama = req.body.nama;
+    daftarMahasiswa[parseInt(req.params.id)-1].jurusan=req.body.jurusan;
+    fs.writeFileSync('./data.json',JSON.stringify(daftarMahasiswa));
+    res.json(mahasiswa);
+})
+
+//DELETE
+app.delete('/mahasiswa/:id',(req,res)=>{
+    const mahasiswa = daftarMahasiswa.find(data=>data.id === parseInt(req.params.id));
+
+    //jika data mhs yang akan dihapus tidak ada
+    if(!mahasiswa) res.status(404).send('Data mahasiswa dengan ID tersebut tidak ditemukan !');
+
+    //jika ditemukan 
+    delete daftarMahasiswa[parseInt(req.params.id)-1];
+    fs.writeFileSync('./data.json',JSON.stringify(daftarMahasiswa));
+    res.send("Data berhasil dihapus!");
+})
+
+app.listen(5000,()=> console.log("Listening on Port 5000..."));
